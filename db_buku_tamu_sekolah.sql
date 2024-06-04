fc7e56dfe0a97b256b23fad86702d94bb8124d87
Feerse <sungaikeledang6@gmail.com>
2024-06-04 08:15:02 +0800

    Rename database

--- db_buku_tamu_sekolah.sql
+++ db_buku_tamu_sekolah.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_buku_tamu`
+-- Database: `db_buku_tamu_sekolah`
 --
 
 -- --------------------------------------------------------

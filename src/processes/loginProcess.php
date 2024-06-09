e761f3ac3846fdb1b094054fc02d3dce4b882466
Feerse <sungaikeledang6@gmail.com>
2024-06-09 16:01:26 +0800

    Refactor login process to use prepared statements for improved security

--- src/processes/loginProcess.php
+++ src/processes/loginProcess.php
@@ -4,8 +4,11 @@ include '../../inc/conn.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
-$result = mysqli_query($conn, $query);
+$query = "SELECT * FROM admin WHERE username=? AND password=?";
+$stmt = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 
 if (mysqli_num_rows($result) > 0) {
     $data = mysqli_fetch_array($result);

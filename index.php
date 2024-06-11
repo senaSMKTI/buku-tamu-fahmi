7dc99d1c69a941ede0c8e5b44d131f94289c1fcc
Feerse <sungaikeledang6@gmail.com>
2024-06-11 10:25:44 +0800

    Update responsiveness

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['id_admin'])) {
 <body class="bg-[url('./public/img/bg.jpg')] bg-no-repeat bg-cover text-gray-900">
     <div class="flex justify-center items-center h-[100vh]">
         <?php if (isset($_GET['url']) && $_GET['url'] == 'tambah-buku-tamu') : ?>
-            <div class="block bg-gray-100 p-10 rounded-md">
+            <div class="block bg-gray-100 p-10 max-md:p-8 rounded-md max-md:w-min">
                 <a href="./index.php" class="inline-flex items-center gap-1 py-1.5 px-2 rounded-md shadow hover:shadow-md border duration-300">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
                         <path fill-rule="evenodd" d="M12.5 9.75A2.75 2.75 0 0 0 9.75 7H4.56l2.22 2.22a.75.75 0 1 1-1.06 1.06l-3.5-3.5a.75.75 0 0 1 0-1.06l3.5-3.5a.75.75 0 0 1 1.06 1.06L4.56 5.5h5.19a4.25 4.25 0 0 1 0 8.5h-1a.75.75 0 0 1 0-1.5h1a2.75 2.75 0 0 0 2.75-2.75Z" clip-rule="evenodd" />
